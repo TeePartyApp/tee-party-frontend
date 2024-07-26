f94e52accd4318a1a47c364f6dbae1cc8e39ee05
bradzara <bradzara93@gmail.com>
2024-07-26 11:44:35 -0700

    Add unmatch user feature

--- src/Matches/MatchesIndex.jsx
+++ src/Matches/MatchesIndex.jsx
@@ -1,40 +1,67 @@
 import React, { useState, useEffect } from "react";
 import './Matches.css';
+import axios from "axios";
 
 export function MatchesIndex(props) {
-  const [selectedUser, seSelectedUser] = useState(null);
+  const [selectedMatch, setSelectedMatch] = useState(null);
 
   
 
   const handleClick = (match) => {
-    seSelectedUser(match.matched_user);
+    setSelectedMatch(match);
   }
+
+  const handleUnmatch = () => {
+    if (selectedMatch) {
+      axios.delete(`http://localhost:3000/matches/${selectedMatch.id}.json`)
+        .then((response) => {
+          console.log("You have been unmatched from:", response.data);
+          window.location.reload();
+        })
+        .catch(error => {
+          console.error("Error unmatching user:", error);
+        });
+    }
+  };
   
   return (
     <div>
       <h1 className="header">Matches</h1>
-        <div className="matches-container">
-          <div className="profiles">
-            {props.matches.map(match => (
-              <div key={match.id} className="profile" onClick={() => handleClick(match)}>
-                <img src={match.matched_user.image_url} alt={match.matched_user.name} className="profile-image" />
-                <p className="profile-name">{match.matched_user.name}</p>
-              </div>
-            ))}
-          </div>
-          {selectedUser && (
-            <div className="body">
-              <div className="card">
-                <img className="card-img-top" src={selectedUser.image_url} alt={selectedUser.name} />
-                <h2 className="card-title">{selectedUser.name}</h2>
-                <p className="card-text">{selectedUser.info}</p>
-                <div className="card-buttons">
-                  <button className="btn btn-secondary">Message</button>
+      <div className="matches-container">
+        {props.matches.length > 0 ? (
+          <>
+            <div className="profiles">
+              {props.matches.map(match => (
+                <div key={match.id} className="profile" onClick={() => handleClick(match)}>
+                  <img src={match.matched_user.image_url} alt={match.matched_user.name} className="profile-image" />
+                  <p className="profile-name">{match.matched_user.name}</p>
+                </div>
+              ))}
+            </div>
+            {selectedMatch ? (
+              <div className="body">
+                <div className="card">
+                  <img className="card-img-top" src={selectedMatch.matched_user.image_url} alt={selectedMatch.matched_user.name} />
+                  <h2 className="card-title">{selectedMatch.matched_user.name}</h2>
+                  <p className="card-text">{selectedMatch.matched_user.info}</p>
+                  <div className="card-buttons">
+                    <button className="btn btn-secondary">Message</button>
+                    <button className="btn btn-secondary" onClick={handleUnmatch}>Unmatch</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-        </div>
+            ) : (
+              <div className="blank-page-message">
+                Select a user to start chatting!
+              </div>
+            )}
+          </>
+        ) : (
+          <div className="blank-page-message">
+            You do not have any matches, go back to the home page and start liking!
+          </div>
+        )}
+      </div>
     </div>
   );
 }
